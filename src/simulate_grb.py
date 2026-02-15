a956aa41ea7b8e0ab441474d4c77c2ca1f37c7f8
Jhanak Shrestha <jhanakshrestha310@gmail.com>
2026-02-15 12:35:13 +0545

    Increased countand ran again

--- src/simulate_grb.py
+++ src/simulate_grb.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 
-def simulate_grb_events(n=20, t0=1.126e9):
+def simulate_grb_events(n=500, t0=1.126e9):
     times = t0 + np.sort(np.random.uniform(0, 1e4, n))
     fluence = np.random.lognormal(-7, 0.5, n)
     ra = np.random.uniform(0, 2*np.pi, n)
--- src/simulate_gw.py
+++ src/simulate_gw.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 
 
-def simulate_gw_events(n=50, t0=1.126e9):
+def simulate_gw_events(n=1000, t0=1.126e9):
     times = t0 + np.sort(np.random.uniform(0, 1e4, n))
     snr = np.random.normal(10, 2, n)
     ra = np.random.uniform(0, 2*np.pi, n)
